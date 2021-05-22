df35c3a500d9fc64bd5dd44cd1a3a0d99ec831b3
Joseph <64382714+joerack81@users.noreply.github.com>
2021-05-22 14:23:34 +0200

    Add files via upload

---
+++ ArduinoUnoR3.ino
@@ -0,0 +1,148 @@
+/*
+
+1 linea: scrolling
+2 linea: statica
+ 
+ riferimento: http://arduino.cc/en/Tutorial/LiquidCrystal
+ 
+ circuito:
+ * LCD RS pin al digital pin 2
+ * LCD Enable pin al digital pin 3
+ * LCD D4 pin al digital pin 4
+ * LCD D5 pin al digital pin 5
+ * LCD D6 pin al digital pin 6
+ * LCD D7 pin al digital pin 7
+ * LCD R/W pin a ground
+ * 2x resistenza da 390ohm in serie:
+ * finisce a +5V e ground
+ * wiper  LCD VO pin (pin 3)
+*/
+
+// librerie
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// Usa l'indirizzo default I2C
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// A seconda del modello del servo, è necessario sintonizzare questi valori al meglio possibile senza forzare il perno
+
+#define start1  130 // Avvio movimento del servo 1
+#define stop1  300 // Stop movimento del servo 1
+#define start2  110 // Avvio movimento del servo 2
+#define stop2  220 // Stop movimento del servo 2
+
+uint8_t servonum1 = 0; // Servo 1 assegnato alla port 0 di Adafruit servo driver
+uint8_t servonum2 = 1; // Servo 1 assegnato alla port 1 di Adafruit servo driver
+
+// Inizializzazione pin del display a seconda del circuito
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+// Impostazione LCD 16x2
+int screenWidth = 16;
+int screenHeight = 2;
+
+// Prima linea a scorrimento, seconda statica
+// line1 = Scorrimento
+String line1 = "ISTITUTO TECNICO INDUSTRIALE STATALE - G. OMAR – NOVARA 2015/16       Non esistono surrogati del duro lavoro - Thomas Edison";
+// line2 = Statica
+String line2 = " FERRI JOSEPH ";
+
+// Flag di riferimento
+int stringStart, stringStop = 0;
+int scrollCursor = screenWidth;
+
+// Assegnazione pin ai vari Led
+void setup() {
+  lcd.begin(screenWidth,screenHeight);
+  Serial.begin(9600);
+  pinMode (8, OUTPUT); //LED 1 al pin 8
+  pinMode (13, OUTPUT); //LED 2 al pin 13
+  pinMode (12, OUTPUT); //LED 3 al pin 12
+  pwm.begin();
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+  yield();
+}
+
+void loop() {
+  lcd.setCursor(scrollCursor, 0);
+  lcd.print(line1.substring(stringStart,stringStop));
+  lcd.setCursor(0, 1);
+  lcd.print(line2);
+  delay(300);
+  lcd.clear();
+  if(stringStart == 0 && scrollCursor > 0){
+    scrollCursor--;
+    stringStop++;
+  } else if (stringStart == stringStop){
+    stringStart = stringStop = 0;
+    scrollCursor = screenWidth;
+  } else if (stringStop == line1.length() && scrollCursor == 0) {
+    stringStart++;
+  } else {
+    stringStart++;
+    stringStop++;
+  }
+  char c = Serial.read();
+  
+  if (c == 'Y' || c == 'y') //LED 1 on
+  {
+    digitalWrite (8, HIGH);
+  }
+  if (c == 'U' || c == 'u') //LED 1 off
+  {
+    digitalWrite (8, LOW);
+  }
+  
+  if (c == 'G' || c == 'g') //LED 2 on
+  {
+    digitalWrite (13, HIGH);
+  }
+  if (c == 'H' || c == 'h') //LED 2 off
+  {
+    digitalWrite (13, LOW);
+  }
+if (c == 'B' || c == 'b') //LED 3 on
+  {
+    digitalWrite (12, HIGH);
+  }
+  if (c == 'N' || c == 'n') //LED 3 off
+  {
+    digitalWrite (12, LOW);
+  }
+  
+  if (c == 'O' || c == 'o') //Servo 1, 0 a 90
+  {
+    for (uint16_t pulselen = start1; pulselen < stop1; pulselen++) 
+    {
+      pwm.setPWM(servonum1, 0, pulselen);
+      delay(10);
+    }
+  }
+  if (c == 'P' || c == 'p') //Servo 1, 90 a 0?
+  {
+    for (uint16_t pulselen = stop1; pulselen > start1; pulselen--) 
+    {
+      pwm.setPWM(servonum1, 0, pulselen);
+      delay(10);
+    }
+  }
+
+  if (c == 'J' || c == 'j') //Servo 2, 0 a 90?
+  {
+    for (uint16_t pulselen = start2; pulselen < stop2; pulselen++) 
+    {
+      pwm.setPWM(servonum2, 0, pulselen);
+      delay(10);
+    }
+  }
+  if (c == 'K' || c == 'k') //Servo 2, 90 a 0?
+  {
+    for (uint16_t pulselen = stop2; pulselen > start2; pulselen--) 
+    {
+      pwm.setPWM(servonum2, 0, pulselen);
+      delay(10);
+    }
+  }
+}
---
+++ Nano_versionechiuso.ino
@@ -0,0 +1,103 @@
+
+/*=================================================================*/
+/*Dichiaro le variabili*/
+/*=================================================================*/
+#include <LiquidCrystal_I2C.h>         //Libreria per pilotare il display LCD con solo 2 fili 
+#define pin_temp A2                   //Pin di collegamento del piedino Vout del sensore di temperatura
+float temp = 0;                      //Variabile in cui verrà memorizzata la temperatura rilevata
+int tip120PIN = 10; 
+LiquidCrystal_I2C lcd(0x3F,16,2);   //Inizializzazione della libreria con i pin I2C per il displayLCD
+int pinSensorPIR = 5;               //Variabile sensore PIR
+int pinLed = 9;                     //Variabile led garage
+
+int valorSensorPIR = 0;              //Imposto un valore basso al sensore PIR
+/*=================================================================*/
+/*Setup*/
+/*=================================================================*/
+void setup() {
+  lcd.init();    //Impostazione del display LCD
+  lcd.backlight();
+  lcd.setCursor(0, 0);     //Sposto il cursore sulla prima riga (riga 0) e sulla prima colonna
+  lcd.print("Temperatura:");    //Stampo il messaggio 'Temperatura:' sulla prima riga
+//Imposto Vref dell'ADC a 1,1V
+
+analogReference(INTERNAL);
+  Serial.begin(9600); //Inizializzo il seriale
+ 
+
+/*=================================================================*/
+  /*Definisco i pin come input e output*/
+/*=================================================================*/
+  pinMode(tip120PIN, OUTPUT);  // analogWrite(tip120PIN,200);
+  pinMode(pinSensorPIR,INPUT);
+  pinMode(pinLed,OUTPUT);
+  pinMode(pin_temp,INPUT);
+  delay(6000);
+}
+/*=================================================================*/
+/*Il loop: dove il codice viene eseguito*/
+/*=================================================================*/ 
+void loop() {  
+//Calcolo temperatura
+temp = 0;
+for (int i = 0; i < 5; i++) {                  //Esegue l'istruzione successiva 5 volte
+temp += (analogRead(pin_temp) / 9.31);         //Calcola la temperatura e la somma alla variabile 'temp'
+}
+temp /= 5;                                    //Calcola la media matematica dei valori di temperatura
+
+/*=================================================================
+Imposto una condizione sul transistor TP120: quando la temperatura è inferiore a 30°, la tensione e 0
+quando la temperatura è => 30°, viene regolata al 50% dell'alimentazione impiegata (in questo caso 4,5V), 
+quando la temperatura supera i 35°, viene regolata una tensione massima
+=================================================================*/
+
+if (temp <30) {
+ analogWrite(tip120PIN,0);
+ lcd.setCursor(1, 1);    
+ lcd.print("Duty cycle  0% ");
+}  else if ((temp >=29) && (temp <35))
+{  analogWrite(tip120PIN,123);
+  lcd.setCursor(1, 1);    
+ lcd.print("Duty cycle 50% ");
+} else {
+  analogWrite(tip120PIN,250);
+  lcd.setCursor(1, 1);    
+ lcd.print("Duty cycle 100%");
+}
+  
+/*Visualizzo la temperatura sul display LCD */
+lcd.setCursor(12, 0); //Sposto il cursore sulla prima colonna e sulla seconda riga
+lcd.print(temp); //Stampo sul display LCD la temperatura
+lcd.print(" C"); //Stampo uno spazio e il carattere 'C' sul display
+
+delay(1000); //Ritardo di un secondo (può essere modificato)
+  //Leggo il valore del sensore PIR, che può assumere 1 o 2 valori
+  //1 quando rileva movimento e 0 quando non c'è movimento.
+  valorSensorPIR = digitalRead(pinSensorPIR);
+   
+  Serial.print("Valore del sensore PIR: ");  
+  Serial.println(valorSensorPIR);
+   
+  //Verificando l'occorrenza di movimento
+  if (valorSensorPIR == 1) {
+    accendi();
+  } else {
+    spegni();
+  }    
+}
+ /*Imposto la funzione accendi e spegni allarme (che poi si occuperebbe soltanto del led)*/
+ 
+void accendi() {
+  digitalWrite(pinLed, HIGH);  //accendo il led
+   
+   
+  delay(6000); //Tempo in cui il led rimane acceso
+   
+  spegni();
+  delay(2000);
+}
+ 
+void spegni() {
+  digitalWrite(pinLed, LOW); //Spengo il led
+   
+}
